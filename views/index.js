59a6718dfb5fe871d75d2c5c17ea0784a21ee9ef
SiapaSangka <156079487+HindiaFtNpc@users.noreply.github.com>
2024-01-18 06:46:26 +0700

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,11 +1,22 @@
-window.onload = () => {
-let chat = document.querySelector('div.container-fluid')
-function addMsg(obj) {
-  let html = document.createElement('span')
-  html.className = 'msg'
-  html.innerHTML = obj
-  chat.appendChild(html)
-}
+const axios = require('axios');
+const os = require('os');
 
-window.onclick = () => addMsg(12)
-}
+const discordWebhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';
+
+// Mendapatkan informasi IP dan spesifikasi hosting
+const ipAddress = require('ip').address();
+const hostname = os.hostname();
+const platform = os.platform();
+const arch = os.arch();
+const cpus = os.cpus();
+
+// Mengirimkan detail hosting ke Discord Webhook
+axios.post(discordWebhookUrl, {
+    content: `Server is running!\n\nIP Address: ${ipAddress}\nHostname: ${hostname}\nPlatform: ${platform}\nArchitecture: ${arch}\nCPUs: ${cpus.length} cores`,
+})
+    .then(response => {
+        console.log('Webhook request sent successfully:', response.data);
+    })
+    .catch(error => {
+        console.error('Error sending webhook request:', error.message);
+    });
