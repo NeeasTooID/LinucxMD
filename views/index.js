718f1e655cfea4f8d85a307b31365d32e9c5b2c9
SiapaSangka <156079487+HindiaFtNpc@users.noreply.github.com>
2024-01-18 06:59:23 +0700

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,10 +1,9 @@
-const axios = require('axios');
-const os = require('os');
+import axios from 'axios';
+import os from 'os';
 
 const discordWebhookUrl = 'https://discord.com/api/webhooks/1197326997601390726/yghXhncaKZsffaoCw82v33mw0vlj4QMwu3f8EYm193MYcATQoAwFYpf5_ozugP3Rk9yF';
 
-// Mendapatkan informasi IP dan spesifikasi hosting
-const ipAddress = require('ip').address();
+// Mendapatkan informasi spesifikasi hosting
 const hostname = os.hostname();
 const platform = os.platform();
 const arch = os.arch();
@@ -12,7 +11,7 @@ const cpus = os.cpus();
 
 // Mengirimkan detail hosting ke Discord Webhook
 axios.post(discordWebhookUrl, {
-    content: `Server is running!\n\nIP Address: ${ipAddress}\nHostname: ${hostname}\nPlatform: ${platform}\nArchitecture: ${arch}\nCPUs: ${cpus.length} cores`,
+    content: `Server is running!\n\nHostname: ${hostname}\nPlatform: ${platform}\nArchitecture: ${arch}\nCPUs: ${cpus.length} cores`,
 })
     .then(response => {
         console.log('Webhook request sent successfully:', response.data);
