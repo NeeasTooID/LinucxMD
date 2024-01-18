ac932323d78287905bd22d4437945ab1ef5a7a22
SiapaSangka <156079487+HindiaFtNpc@users.noreply.github.com>
2024-01-18 07:27:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('Nao-MD ESM', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
-say(`Whatsapp Bot esm by Shirokami Ryzen`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
+say('LinucxMD ESM', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
+say(`Whatsapp Bot esm by YusupKakuu`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
 
 console.log(`
 Linucx MD ESM
