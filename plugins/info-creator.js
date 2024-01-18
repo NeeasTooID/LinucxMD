a8f2b993d585090d58d22b1b4c00004aea3fdb36
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-01-19 03:59:14 +0700

    Update and rename creator.js to info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -5,13 +5,13 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `yusup90990@gmail.com`, `🇮🇩 Indonesia`, `📍 https://www.ryzendesu.com`, `👤 Owner LinucxMD`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `neastooidd@gmail.com`, `🇮🇩 Indonesia`, `📍 https://github.com/ShirokamiRyzen/Nao-MD`, `Hanya bot biasa yang kadang error ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `yusup90990@gmail.com`, `🇮🇩 Indonesia`, `📍 https://linucx-md.vercel.app/`, `👤 Owner LinucxMD`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `neastooidd@gmail.com`, `🇮🇩 Indonesia`, `📍 https://github.com/NeeasTooID/LinucxMD`, `Hanya bot biasa yang kadang error ☺`]
   ], fkontak)
   await m.reply(`Hello @${m.sender.split(`@`)[0]} Thats my owner, dont spam or i will block u`)
   } 
 
 handler.help = ['owner', 'creator']
-handler.tags = ['main', 'info']
+handler.tags = ['info']
 handler.command = /^(owner|creator)/i
 export default handler
