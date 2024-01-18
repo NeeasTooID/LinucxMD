d2a774f876267b5132360dbdd0fbec3dc2aaf7a6
SiapaSangka <156079487+HindiaFtNpc@users.noreply.github.com>
2024-01-18 07:30:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/* If You Copy, Don`t Delete This Credit!!! 
-  Don`t Sell This Script Or I Take Immediately 
-  Yang Jual Script Ini Report/Hangusin Aja Akunnya Atau Pukulin ae orangnya
-  Move To Pairing Code
-  Buat Yg Nggk muncul Codenya Itu Disebabkan Oleh Banyaknya Plugins
-  Jika Ingin Mengambil Sesi, Backup Semua File Plugins & Hapus Semua File Plugins
-  Setelah Sudah Kalian Bisa Mengembalikan Semua File Pluginsnya Agar Bisa Dipakai
-  Regards from YanXiao ♡
-*/
-
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import './config.js'
 
@@ -110,10 +100,10 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 
 const store = useStore ? makeInMemoryStore({ level: 'silent' }) : undefined
 
-store?.readFromFile('./nao_store.json')
+store?.readFromFile('./lstore.json')
 // save every 10s
 setInterval(() => {
-	store?.writeToFile('./nao_store.json')
+	store?.writeToFile('./lstore.json')
 }, 10_000)
 
 const { version, isLatest} = await fetchLatestBaileysVersion()
