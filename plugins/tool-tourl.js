31685ed768da244e90dbd7b17a1cc3d753b7f7be
SiapaSangka <156079487+HindiaFtNpc@users.noreply.github.com>
2024-01-19 07:20:01 +0700

    Add files via upload

--- plugins/tool-tourl.js
+++ plugins/tool-tourl.js
@@ -1,22 +1,30 @@
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 
-let handler = async (m) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+let loadd = [
+ '> ■ □ □ □ □ □ □ □ □ □ □ 10%',
+ '> ■ ■ ■ □ □ □ □ □ □ □ □ 40%',
+ '> ■ ■ ■ ■ ■ ■ □ □ □ □ □ 60%',
+ '> ■ ■ ■ ■ ■ ■ ■ ■ ■ □ □ 80%',
+ '> ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■100%',
+ 'Loading Complate!...'
+ ]
+ 
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'No media found'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`📮 *L I N K :*
-${link}
-📊 *S I Z E :* ${media.length} Byte
-📛 *E x p i r e d :* ${isTele ? 'No Expiry Date' : 'Unknown'}`)
+  m.reply(`
+*Link:* ${link}
+*Size:* ${media.length} Byte
+*Expired:* ${isTele ? 'No Expiry Date' : 'Unknown'}
+Npnpicyy❤️‍🩹`)
 }
-
-handler.help = ['upload (reply media)', 'tourl (reply media)']
+handler.help = ['upload']
 handler.tags = ['tools']
 handler.command = /^(tourl|upload)$/i
-handler.limit = false
-handler.register = true
+handler.limit = true
 export default handler
