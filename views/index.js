4dee6cc81fc506ca2a6c6b05ea6c21e7be39ce6e
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-02-02 15:01:16 +0700

    Update index.js

--- views/index.js
+++ views/index.js
@@ -4,31 +4,33 @@ import os from 'os';
 const discordWebhookUrl = 'https://discord.com/api/webhooks/1156469297003110470/oLAK50KoPtAhvWQz9KgDplSFHbVOiFXLL8Fp8rDhrjP6eSid_x4t4-fE6i5yNy9CS5gM';
 
 // Mendapatkan informasi spesifikasi hosting
-const hostname = os.hostname();
 const platform = os.platform();
 const arch = os.arch();
 const cpus = os.cpus();
 
 // Membuat pesan dengan embed
-const embedMessage = {
-    embeds: [{
-        title: 'Server Information',
-        description: 'Website is Update!',
-        fields: [
-            { name: 'Hostname', value: `||${hostname}||`, inline: true },
-            { name: 'Platform', value: platform, inline: true },
-            { name: 'Architecture', value: arch, inline: true },
-            { name: 'CPUs', value: `${cpus.length} cores`, inline: true },
-        ],
-        color: 0x00FF00, // Warna hijau (Anda dapat mengganti sesuai keinginan)
-    }],
+const createEmbedMessage = (changes) => {
+    return {
+        embeds: [{
+            title: 'Server Information',
+            description: 'Website is Updated!',
+            fields: [
+                { name: 'Platform', value: platform, inline: true },
+                { name: 'Architecture', value: arch, inline: true },
+                { name: 'CPUs', value: `${cpus.length} cores`, inline: true },
+            ],
+            color: 0x00FF00, // Warna hijau (Anda dapat mengganti sesuai keinginan)
+        }],
+    };
 };
 
 // Mengirimkan detail hosting ke Discord Webhook dengan pesan embed
-axios.post(discordWebhookUrl, embedMessage)
-    .then(response => {
-        console.log('Webhook request sent successfully:', response.data);
-    })
-    .catch(error => {
-        console.error('Error sending webhook request:', error.message);
-    });
+const sendWebhook = (message) => {
+    axios.post(discordWebhookUrl, message)
+        .then(response => {
+            console.log('Webhook request sent successfully:', response.data);
+        })
+        .catch(error => {
+            console.error('Error sending webhook request:', error.message);
+        });
+};
