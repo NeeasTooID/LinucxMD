cc91d3d1fe272ab94728e7e7df5cc8c008c568c1
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-02-02 17:07:27 +0700

    Update and rename speed.js to info-speed.js

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -82,10 +82,10 @@ _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `, fakes)
 }
-handler.help = ['ping', 'speed']
-handler.tags = ['info', 'tools']
+handler.help = ['ping']
+handler.tags = ['info']
 
-handler.command = /^(ping|speed|info)$/i
+handler.command = /^(ping)$/i
 export default handler
 
 const more = String.fromCharCode(8206)
