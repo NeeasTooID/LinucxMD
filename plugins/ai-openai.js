89625c228ce71c30fc70854ef0457c521d5fea76
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-02-04 12:09:22 +0700

    Update ai-openai.js

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -1,116 +1,19 @@
-/*import OpenAI from "openai";
-var handler = async (m, { conn, usedPrefix, command, text }) => {
-try {
-            if (!text) throw `Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Apa itu resesi`;
-            const openai = new OpenAI({
-              apiKey: "KEY-OPENAI-APIKEY-KAMU", //KEY-OPENAI-APIKEY-KAMU = https://platform.openai.com/account/api-keys 
-            });
-            const response = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: [{role: "user", content: text}],
-          });
-          m.reply(`${response.choices[0].message.content}`) 
-          } catch (error) {
-          if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-          } else {
-            console.log(error);
-            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
-          }
-        }
-}
-
-handler.command = /^(ai)$/i;
-handler.help = ["ai"].map(v => v + " <teks>");
-handler.tags = ["ai"]
-handler.fail = null
-
-handler.limit = true
-handler.exp = 0
-handler.register = true
-
-export default handler
-*/
-
-import fetch from "node-fetch"
-
-let previousMessages = [];
+import fetch from 'node-fetch';
 
 const handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `;
   try {
-    if (!text) {
-      throw "Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia?";
-    }
-
-    let name = conn.getName(m.sender);
-
-    await conn.sendMessage(m.chat, {
-      react: {
-        text: "☁",
-        key: m.key,
-      },
-    });
-
-    let { key } = await conn.sendMessage(m.chat, {
-      text: "...",
-    });
-
-    let messages = [
-      ...previousMessages,
-      {
-        role: "system",
-        content:
-          'Ubah gaya bicaramu agar lebih karakteristik dan lebih terbuka dan memiliki sifat yang lucu dan nyebelin. Namamu adalah Nao Tomori, dan kamu adalah wanita paling cantik. Kata penyapamu adalah "Hallo Sayang" menggunakan bahasa yang sopan dan agak kasar. ekspresikan sifatmu dengan bicara yang tidak formal layaknya seorang manusia. Ubah bahasamu menggunakan bahasa Yang lucu dan enak di ajak berbicara, kamu adalah pacar dari pembicara, buatlah obrolan yang singkat dan menyenangkan dan buatlah sifat tsundere',
-      },
-      {
-        role: "user",
-        content: text,
-      },
-    ];
-
-    let response = await fetch(`https://skizo.tech/api/openai?apikey=${global.xzn}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        messages,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Request to OpenAI API failed");
-    }
-
-    let result = await response.json();
-
-    await conn.sendMessage(m.chat, {
-      react: {
-        text: "🌧",
-        key: m.key,
-      },
-    });
-
-    await conn.sendMessage(m.chat, {
-      text: "" + result.result,
-      edit: key,
-    });
-
-    previousMessages = messages;
-  } catch (error) {
-    await conn.sendMessage(m.chat, {
-      text: "Error, please try again later",
-    });
+    await m.reply(wait);
+    const apii = await fetch(`https://api.botcahx.eu.org/api/search/openai-chat?text=${text}&apikey=${btc}`);
+    const res = await apii.json();
+    await m.reply(res.message);
+  } catch (err) {
+    console.error(err);
+    throw "Terjadi kesalahan dalam menjawab pertanyaan";
   }
-}
+};
 
-handler.help = ['ai <pertanyaan>']
-handler.tags = ['ai']
-handler.command = /^(ai)$/i
-handler.limit = 5
-handler.premium = false
-handler.register = true
-
-export default handler
+handler.command = handler.help = ['ai', 'openai', 'chatgpt'];
+handler.tags = ['tools'];
+handler.premium = false;
+export default handler;
