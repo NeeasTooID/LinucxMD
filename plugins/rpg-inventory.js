0ee8fb079ffe00d369e37d1c399cb0d1323b9134
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-02-29 08:04:39 -0800

    Rpg Kanjut🦍

---
+++ plugins/rpg-inventory.js
@@ -0,0 +1,147 @@
+import daily from './rpg-daily.js'
+import weekly from './rpg-weekly.js'
+import monthly from './rpg-monthly.js'
+import adventure from './rpg-adventure.js'
+
+const inventory = {
+  others: {
+    health: true,
+    money: true,
+    exp: true,
+    limit: true,
+    level: true,
+  },
+  items: {
+    potion: true,
+    trash: true,
+    wood: true,
+    rock: true,
+    string: true,
+    emerald: true,
+    diamond: true,
+    gold: true,
+    iron: true,
+    upgrader: true,
+    pet: true,
+  },
+  durabi: {
+    sworddurability: true,
+    pickaxedurability: true,
+    fishingroddurability: true,
+    armordurability: true,
+  },
+  tools: {
+    armor: {
+      '0': '❌',
+      '1': 'Leather Armor',
+      '2': 'Iron Armor',
+      '3': 'Gold Armor',
+      '4': 'Diamond Armor',
+      '5': 'Emerald Armor',
+      '6': 'Crystal Armor',
+      '7': 'Obsidian Armor',
+      '8': 'Netherite Armor',
+      '9': 'Wither Armor',
+      '10': 'Dragon Armor',
+      '11': 'Hacker Armor'
+    },
+    sword: {
+      '0': '❌',
+      '1': 'Wooden Sword',
+      '2': 'Stone Sword',
+      '3': 'Iron Sword',
+      '4': 'Gold Sword',
+      '5': 'Copper Sword',
+      '6': 'Diamond Sword',
+      '7': 'Emerald Sword',
+      '8': 'Obsidian Sword',
+      '9': 'Netherite Sword',
+      '10': 'Samurai Slayer Green Sword',
+      '11': 'Hacker Sword'
+    },
+    pickaxe: {
+      '0': '❌',
+      '1': 'Wooden Pickaxe',
+      '2': 'Stone Pickaxe',
+      '3': 'Iron Pickaxe',
+      '4': 'Gold Pickaxe',
+      '5': 'Copper Pickaxe',
+      '6': 'Diamond Pickaxe',
+      '7': 'Emerlad Pickaxe',
+      '8': 'Crystal Pickaxe',
+      '9': 'Obsidian Pickaxe',
+      '10': 'Netherite Pickaxe',
+      '11': 'Hacker Pickaxe'
+    },
+    fishingrod: true,
+
+  },
+  crates: {
+    common: true,
+    uncommon: true,
+    mythic: true,
+    legendary: true,
+  },
+  pets: {
+    horse: 10,
+    cat: 10,
+    fox: 10,
+    dog: 10,
+  },
+  cooldowns: {
+    lastclaim: {
+      name: 'Claim',
+      time: daily.cooldown
+    },
+    lastweekly: {
+    	name: 'Weekly',
+        time: weekly.cooldown
+        },
+    lastmonthly: {
+      name: 'Monthly',
+      time: monthly.cooldown
+    },
+    lastadventure: {
+      name: 'Adventure',
+      time: adventure.cooldown
+    }
+  }
+}
+let handler = async (m, { conn }) => {
+  let user = global.db.data.users[m.sender]
+  const tools = Object.keys(inventory.tools).map(v => user[v] && `${global.rpg.emoticon(v)} ${v}: ${typeof inventory.tools[v] === 'object' ? inventory.tools[v][user[v]?.toString()] : `Level(s) ${user[v]}`}`).filter(v => v).join('\n').trim()
+  const items = Object.keys(inventory.items).map(v => user[v] && `${global.rpg.emoticon(v)} ${v}: ${user[v]}`).filter(v => v).join('\n').trim()
+  const dura = Object.keys(inventory.durabi).map(v => user[v] && `${global.rpg.emoticon(v)} ${v}: ${user[v]}`).filter(v => v).join('\n').trim()
+  const crates = Object.keys(inventory.crates).map(v => user[v] && `${global.rpg.emoticon(v)} ${v}: ${user[v]}`).filter(v => v).join('\n').trim()
+  const pets = Object.keys(inventory.pets).map(v => user[v] && `${global.rpg.emoticon(v)} ${v}: ${user[v] >= inventory.pets[v] ? 'Max Levels' : `Level(s) ${user[v]}`}`).filter(v => v).join('\n').trim()
+  const cooldowns = Object.entries(inventory.cooldowns).map(([cd, { name, time }]) => cd in user && `*• ${name}*: ${new Date() - user[cd] >= time ? '✅' : '❌'}`).filter(v => v).join('\n').trim()
+  const caption = `Nama: ${conn.getName(m.sender)}
+${Object.keys(inventory.others).map(v => user[v] && ` ${v}: ${user[v]}`).filter(v => v).join('\n')}${tools ? `
+Role: ${user.role}
+
+${tools}` : ''}${dura ? `
+
+${dura}` : ''}${items ? `
+
+${items}
+
+Items: ${Object.keys(inventory.items).map(v => user[v]).reduce((a, b) => a + b, 0)} Items` : ''}${crates ? `
+
+${crates}
+
+Crates: ${Object.keys(inventory.crates).map(v => user[v]).reduce((a, b) => a + b, 0)} Crates` : ''}${pets || user.petFood ? `
+
+${pets ? pets + '\n' : ''}${user.petFood ? 'Pet Food: ' + user.petFood : ''}` : ''}${cooldowns ? `
+
+${cooldowns}` : ''}
+
+*• Mining:* ${user.lastmining == 0 ? '✅': '❌'}
+`.trim()
+m.reply(caption)
+}
+handler.help = ['inventory', 'inv']
+handler.tags = ['rpg']
+handler.command = /^(inv(entory)?|bal(ance)?|money|e?xp)$/i
+
+handler.register = true
+export default handler
